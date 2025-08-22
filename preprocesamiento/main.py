4682d9c57fb3e96f7fe40c1900b52ff39b624f1c
Adhara Cavazos <95373792+AdharaMatsu@users.noreply.github.com>
2025-08-21 19:01:25 -0600

    update

--- preprocesamiento/main.py
+++ preprocesamiento/main.py
@@ -4,8 +4,19 @@ with open('tenis.csv', 'r', encoding='utf-8') as f:
     read = csv.reader(f)
     for fila in read:
         #print(fila)
-        lista.append(fila)
+        lista.append(fila[1:])
 
-print(lista)
-print(lista[0])
-print(lista[1][1])
+for i in range(1, len(lista)):
+    if lista[i][1] == 'Sol':
+        lista[i][1] = 1
+    elif lista[i][1] == 'Nubes':
+        lista[i][1] = 2
+    elif lista[i][1] == 'Lluvia':
+        lista[i][1] = 3
+for e in lista:
+    print(e)
+
+print()
+print(lista[1][0])
+#print(lista[0])
+#print(lista[1][1])
