c6da6428a298091e89b00177cdc9630a8e47f661
Adhara Cavazos <95373792+AdharaMatsu@users.noreply.github.com>
2025-09-10 18:49:22 -0600

    update

--- ClasificadorCasa/main.py
+++ ClasificadorCasa/main.py
@@ -22,19 +22,33 @@ for fila in lista:
 lista = toInt(lista)
 
 caso1 = [35,28,90,33,43,11,37,59]
+#caso1 = [28,10,25,82,50,39,60,1]
+#caso1 = [6,3,5,9,7,30,55,77,2]
 # Manhattan
-resultados = list()
+
 for fila in range(len(lista)):
     j = 0
     sumatoria = 0
     for columna in range(len(lista[fila])-1):
         sumatoria += abs(lista[fila][columna]-caso1[j])
         j = j + 1
-    resultados.append(sumatoria)
+    # Cambia los valores
+    lista[fila].append(sumatoria)
 
-print(resultados[0])
 #################
 
+k = 15
 
+ordenada = sorted(lista, key=lambda x: x[-1])
+# Lambda ayuda a definir funciones pequeñas sin nombre
+# x es cada sublista (ej: [3, 5], [1, 9], …).
+# x[-1]  agarra el ultimo elemento de las listas de la lista como
+# criterio para ordenar
 
-
+seleccion = ordenada[:k]
+clases = [fila[-2] for fila in seleccion]
+print(clases)
+maximo = max(clases, key=clases.count)
+for c in casas:
+    if casas[c] == maximo:
+        print('mmmm... ya see., te pondre en... ',c)
